3717107d48edbc92f674a0203a30fbcd49c9f63d
Vlad Nester <pikpikapoc@gmail.com>
2020-03-09 18:29:35 +0300

    add ClientPart & SqlStart

---
+++ SqlStart.sql
@@ -0,0 +1,30 @@
+﻿create database TestTaskDB;
+
+use TestTaskDB;
+
+create table PRODUCTS
+(
+	Id int identity(1,1) primary key,
+	ProductName nvarchar(200),
+	ProductCost float,
+);
+
+create table ORDERS
+(
+	Id int identity(1,1) primary key,
+	OrderStatus nvarchar(10) check(OrderStatus IN(N'формирование',N'ожидание доставки',N'доставлен')),
+	StartDate date not null,
+	EndDate date,
+);
+
+drop table ORDERS;
+
+create table ORDERSPRODUCTS
+(
+	ProductFK int foreign key references Products(Id),
+	OrdersFK int foreign key references Orders(Id)
+);
+
+insert into ORDERS values ('доставлен','03.09.2020',null);
+
+select * from ORDERS;
--- VladNesterTest/ClientPart/src/app/app.module.ts
+++ VladNesterTest/ClientPart/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrderComponent } from './order/order.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
